3750b1c914d9c1b54217a6ef9c36094e260143e7
ChitawornR <chitaworn.rr@gmail.com>
2024-07-27 11:22:39 +0700

    Practice2_Ch04_ReactHook

--- components/ProfileScreen.tsx
+++ components/ProfileScreen.tsx
@@ -1,19 +1,34 @@
-import { StyleSheet, Text, View, Image } from "react-native";
-import React from "react";
-import {styles} from '../styles/styles'
+import { StyleSheet, Text, View, Image, Button } from "react-native";
+import React, { useState } from "react";
+import { styles } from "../styles/styles";
 
 const ProfileScreen = (): React.JSX.Element => {
-  const profileImage = require("../assets/img/profile2.jpg");
+  const [name, setName] = useState("Chittaworn Khirieksathit");
+  const profileImage1 = require('../assets/img/profile2.jpg');
+  const profile2 = require('../assets/img/profile3.jpg')
+  const [img,setImg] = useState(profileImage1)
+
+  const handleChangeName = () => {
+    setName("Riew");
+  };
+  const handleChangeImage = () => {
+    setImg(profile2)
+  };
+  
 
   return (
     <View style={styles.container}>
       <View style={styles.profileContainer}>
-        <Image source={profileImage} style={styles.profileImage} />
-        <Text style={styles.profileName}>Chittaworn Khirieksathit</Text>
+        <Image source={img} style={styles.profileImage} />
+        <View>
+          <Text style={styles.profileName}>{name}</Text>
+          <Button title="CHANGE NAME" onPress={handleChangeName} />
+          <Text>{"\n"}</Text>
+          <Button title="CHANGE IMAGE" onPress={handleChangeImage} />
+        </View>
       </View>
     </View>
   );
 };
 
 export default ProfileScreen;
-
