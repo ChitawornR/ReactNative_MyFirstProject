1ea42fb73a7a7de2b065c5ce8bc8484bca992e0e
ChitawornR <chitaworn.rr@gmail.com>
2024-08-31 11:59:39 +0700

    practice 3

--- App.tsx
+++ App.tsx
@@ -1,23 +1,45 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import {NavigationContainer} from '@react-navigation/native'
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import HomeScreen from "./screens/HomeScreen";
 import AboutScreen from "./screens/AboutScreen";
 import CreatePostScreen from "./components/CreatePostScreen";
 
-const HomeStack = createNativeStackNavigator()
+const HomeStack = createNativeStackNavigator();
 
-const App = ():React.JSX.Element => {
+const App = (): React.JSX.Element => {
   return (
-      <NavigationContainer>
-        <HomeStack.Navigator initialRouteName="Home">
-          <HomeStack.Screen name="Home" component={HomeScreen}/>
-          <HomeStack.Screen name="About" component={AboutScreen}/>
-          <HomeStack.Screen name="CreatePost" component={CreatePostScreen}/>
-        </HomeStack.Navigator>
-      </NavigationContainer>
+    <NavigationContainer>
+      <HomeStack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: { backgroundColor: "#20b2aa" },
+          headerTintColor: "white",
+          headerTitleStyle: { fontWeight: "bold" },
+          // headerTitleAlign: "center",
+        }}
+      >
+        <HomeStack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "HomePage" }}
+        />
+        <HomeStack.Screen
+          name="About"
+          component={AboutScreen}
+          options={{
+            title:'About Us',
+          // headerStyle:{backgroundColor:'#20b2aa'},
+          // headerTintColor:'white',
+          // headerTitleStyle:{fontWeight:'bold'},
+          headerTitleAlign:'center'
+          }}
+        />
+        <HomeStack.Screen name="CreatePost" component={CreatePostScreen} />
+      </HomeStack.Navigator>
+    </NavigationContainer>
   );
 };
 
